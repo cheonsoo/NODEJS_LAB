2f42ef50c039d983f6b12e286de8cf2e163fce6e
chance <cs.park@gsshop.com>
2020-09-26 02:58:58 +0900

    .

--- app/_algorithm/factorial/factorialWithMemoization.js
+++ app/_algorithm/factorial/factorialWithMemoization.js
@@ -1,22 +1,23 @@
 const start = new Date().getTime();
 
 function factorial(i) {
-    let memo = {};
+  let memo = {};
 
-    function fact(i) {
-        if (i === 0) {
-            return 1;
-        }
-        let value = memo[i - 1] || fact(i - 1);
-        if (!memo[i - 1]) {
-            memo[i - 1] = value;
-        }
-        return i * value;
+  function fact(i) {
+    if (i === 0) {
+      return 1;
     }
+    let value = memo[i - 1] || fact(i - 1);
+    if (!memo[i - 1]) {
+      memo[i - 1] = value;
+    }
+    return i * value;
+  }
 
-    return fact(i);
+  return fact(i);
 }
 
-console.log(factorial(100));
+// console.log(factorial(10000));
+console.log(factorial(5));
 
 console.log(`exe time : ${new Date().getTime() - start}`);
